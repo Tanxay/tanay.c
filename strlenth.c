9436472ff8459690803ab8fdae0fa99bb9f52323
Tanxay <125134137+Tanxay@users.noreply.github.com>
2023-04-28 01:19:24 +0530

    Create strlenth.c

---
+++ strlenth.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+void main()
+{
+ int num, sum = 0;
+ char str[20];
+ scanf("%s",str);
+ num = strlen(str);
+
+ printf("%d",num);
+}
