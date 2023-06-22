3f49ad32dc87c9d9062e30b61056dcf1b6cd6501
Tanxay <125134137+Tanxay@users.noreply.github.com>
2023-06-22 23:54:03 +0530

    Update reversearr.c

--- reversearr.c
+++ reversearr.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+#include  <stdio.h>
+#include <string.h>
+int main(void)
 {
-   int n;
-   int a[1000];
-   scanf("%d",&n);
-   for(int i=0;i<n;i++)
-   {
-     scanf("%d",&a[i]);
-   }
-   for(int i=n-1;i>=0;i--)
-   {
-     printf("%d ",a[i]);
-   }
+    char str[10];
+    scanf("%s",str);
+    printf("Reverse the String:");
+    int len = strlen(str);
+    for(int i=len-1;i>=0;i--)
+    { 
+        printf("%c",str[i]);
+    }  
+    
     return 0;
 }
